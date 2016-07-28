c850217cc269f7d417bbdc81e03c0135dcfdd079
avmaxim <avmax19@gmail.com>
2016-07-28 17:20:57 +0300

    Amend impl for chaining in Async task #2

--- async-tasks/tests/index-test.js
+++ async-tasks/tests/index-test.js
@@ -5,38 +5,20 @@
 
 var expect = chai.expect;
 
-
 describe('Asynchronous Tasks', function() {
 
-    it('should call render array of objects', function() {
+    it('should result an array of 4 stores with promises', function() {
         var vm = asyncTask;
 
         //input
         var addr1 = "blabla 1";
         var addr2 = "lollol 2";
-
-        var storesDatabase = [];
+        
         var addresses = [addr1, addr2];
 
         Promise.all( addresses.map( vm.getZip ) )
-                .then( (zips) => {
-                    return Promise.all( zips.map( vm.getStores ) );
-                })
-                .then ( (stores) => {
-                    return vm.render([].concat.apply([], stores));
-                });
-
-        /*
-
-         Promise.all( addresses.map( vm.getZip ) )
-         .all( (zips) => zips.map( vm.getStores ) )
-         .then( function() {
-         storesDatabase.push(stores);
-         storesDatabase = [].concat.apply([], storesDatabase);
-         return vm.render(storesDatabase);
-         });
-
-         */
+                .then( zips => Promise.all( zips.map( vm.getStores ) ) )
+                .then ( stores => vm.render([].concat.apply([], stores)) );
     });
 
 });
