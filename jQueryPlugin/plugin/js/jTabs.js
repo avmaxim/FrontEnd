0cc71212a08c3a670d0a2a05f1d095ff256720a0
avmaxim <avmax19@gmail.com>
2016-08-01 12:41:12 +0300

    Refactored JTabs Plugin according to Denis's refactoring suggestions.

--- jQueryPlugin/plugin/js/jTabs.js
+++ jQueryPlugin/plugin/js/jTabs.js
@@ -2,9 +2,9 @@
  * Created by andrei.maksimchanka on 7/22/2016.
  */
 
-(function($) {
+(function ($) {
 
-    $.fn[ 'jtabs' ] = function( options ) {
+    $.fn['jtabs'] = function (options) {
         return $.data(this, 'jtabs', new JTabsPlugin(this, options));
     };
 
@@ -12,9 +12,14 @@
 
         constructor(element, options) {
             this.settings = this.initSettings(options);
-            this.$widget = $(element).addClass('jtabs-widget');                                                             // div.jtabs-widget
-            this.$tabControl = this.$widget.children('ul').first().addClass('jtabs-nav');                                   // ul.jtabs-nav
-            this.$tabs = this.$tabControl.children('li').addClass('jtab');                                                  // [li.jtab, ...]
+            this.$widget = $(element).addClass('jtabs-widget');                                                         // div.jtabs-widget
+            this.$tabControl = this.$widget
+                .children('ul')
+                .first()
+                .addClass('jtabs-nav');                                                                                 // ul.jtabs-nav
+            this.$tabs = this.$tabControl
+                .children('li')
+                .addClass('jtab');                                                                                      // [li.jtab, ...]
             this.init();
         };
 
@@ -31,7 +36,7 @@
         init() {
             this.initProgressBar();
             this.initTabsWithContent();
-            this.settings.closeable &&  this.initClosingTabsFeature();
+            this.settings.closeable && this.initClosingTabsFeature();
             this.settings.dynamicTabs.length && this.initDynamicTabsFeature();
             this.settings.urlRouting ? this.initUrlRoutingFeature() : this.changeActiveTabTo(this.$tabs.first());
         }
@@ -44,16 +49,13 @@
         initTabsWithContent() {
             const self = this;
             this.$tabs.find('a').addClass('jlink');
-            this.$tabs
-                .each(function () {
-                    getContentForTab($(this)).addClass('jtabs-content').hide();
-                })
-                .on('click', 'a.jlink', function (e) {
-                    if (!self.settings.urlRouting) {
-                        self.changeActiveTabTo($(this).parent());
-                        e.preventDefault();
-                    }
-                });
+            getContentForTab( this.$tabs ).addClass('jtabs-content').hide();
+            this.$tabs.on('click', 'a.jlink', function (e) {
+                if (!self.settings.urlRouting) {
+                    self.changeActiveTabTo($(this).parent());
+                    e.preventDefault();
+                }
+            });
         }
 
         //**** Closeable Tab Feature ****//
@@ -66,11 +68,14 @@
                 if ($visibleTabs.length > 0) {
                     const $newActiveTab = $visibleTabs;
 
-                    self.changeActiveTabTo( $newActiveTab );
+                    self.changeActiveTabTo($newActiveTab);
                     $tab.addClass('closed').hide();
 
                     let $activeLink = $newActiveTab.find('.jlink');
-                    if ($activeLink.hasClass('dynamic') ) $activeLink.trigger('click');
+
+                    if ($activeLink.hasClass('dynamic')) {
+                        $activeLink.trigger('click');
+                    }
                 }
             });
         }
@@ -78,43 +83,55 @@
         initDynamicTabsFeature() {
             const self = this;
             this.settings.dynamicTabs.forEach(function (tab) {
-                if  (!self.$tabs[tab.number - 1]) return;
+                if (!self.$tabs[tab.number - 1]) {
+                    return;
+                }
 
-                self.$tabs.eq(tab.number - 1).find('.jlink').addClass('dynamic').on('click', function () {
-                    if (!$('#tab' + tab.number).html().trim())  self.loadContentForTab(tab);                             //if content hasn't been loaded yet, do it now.
-                });
+                self.$tabs
+                    .eq(tab.number - 1)
+                    .find('.jlink')
+                    .addClass('dynamic')
+                    .on('click', function () {
+                        if (!$('#tab' + tab.number).html().trim()) {
+                            self.loadContentForTab(tab);                                                                //if content hasn't been loaded yet, do it now.
+                        }
+                    });
             });
         }
 
-        loadContentForTab( tab ) {
-            const self = this;
+        loadContentForTab(tab) {
             this.showProgressBar();
 
-            if ( tab.load ){
-                let nextStep = tab.load(callback);
-                if ( nextStep instanceof Promise) nextStep.then(callback);
-            }
-            else if ( tab.url ) {
-                setTimeout(() => {
-                    $.ajax(tab.url || '').done(callback).fail((response) => callback(response.responseText));
-                }, Math.random() * 3000);
-            }
-            function callback(html){
-                $('#tab' + +tab.number).html(html);
-                self.hideProgressBar();
+            if (tab.load) {
+                let nextStep = tab.load(this.renderTab.bind(this, tab));
+                if (nextStep instanceof Promise) {
+                    nextStep.then(this.renderTab.bind(this, tab));
+                }
+            } else if (tab.url) {
+                $.ajax(tab.url || '')
+                    .then(this.renderTab.bind(this, tab))
+                    .catch((response) => this.renderTab(tab, response.responseText));
             }
         }
 
+        renderTab(tab, html) {
+            $('#tab' + +tab.number).html(html);
+            this.hideProgressBar();
+        }
+
         initUrlRoutingFeature() {
-            const self = this;
-            $(window).on('hashchange', function (){
-                const tabFromHash = self.$tabs.find('[href="' + location.hash + '"]').parent()[0],
-                    $tab = tabFromHash ? $(tabFromHash) : self.$tabs.first();
+            $(window).on('hashchange', () => {
+                const tabFromHash = this.$tabs
+                        .find('[href="' + location.hash + '"]')
+                        .parent()[0],
+                    $tab = tabFromHash ? $(tabFromHash) : this.$tabs.first();
 
-                self.changeActiveTabTo($tab);
+                this.changeActiveTabTo($tab);
                 let $activeLink = $tab.find('.jlink');
-                if ($activeLink.hasClass('dynamic') ) $activeLink.trigger('click');
 
+                if ($activeLink.hasClass('dynamic')) {
+                    $activeLink.trigger('click');
+                }
             }).trigger('hashchange');
         }
 
@@ -123,20 +140,26 @@
                 this.$activeTab.removeClass('active');
                 this.$activeContent.hide();
             }
-            this.$activeTab = $tab;
-            this.$activeContent = getContentForTab($tab);
 
-            location.hash = this.settings.urlRouting ? getHashFromTab(this.$activeTab) : '';
+            this.$activeTab = $tab;
+            this.$activeContent = getContentForTab($tab).first();
+
+            location.hash = this.settings.urlRouting ? getHashFromTab(this.$activeTab)[0] : '';
 
             this.$activeTab.addClass('active');
             this.$activeContent.show();
         }
 
-        showProgressBar () { this.$progressBar.removeClass('hidden'); }
-        hideProgressBar () { this.$progressBar.addClass('hidden'); }
+        showProgressBar() {
+            this.$progressBar.removeClass('hidden');
+        }
+
+        hideProgressBar() {
+            this.$progressBar.addClass('hidden');
+        }
     }
 
-    let getContentForTab = ($tab) => $( getHashFromTab( $tab ) );
-    let getHashFromTab = ($tab) => $tab.find('.jlink')[0].hash ;
+    let getContentForTab = ($tab) => $( getHashFromTab($tab).map( elem => $(elem)[0] ));
+    let getHashFromTab = ($tab) => $tab.find('.jlink').toArray().map( elem => elem.hash );
 
-})(  jQuery);
+})(jQuery);
