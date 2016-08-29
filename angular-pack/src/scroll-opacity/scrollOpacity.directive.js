d6a940f6c26fc0cf189d5f2459a807fa56f3dd40
avmaxim <avmax19@gmail.com>
2016-08-29 15:38:05 +0300

    Get rid of opacity on elements in opacityArea when scroll encounters an entire bottom of the screen.

--- angular-pack/src/scroll-opacity/scrollOpacity.directive.js
+++ angular-pack/src/scroll-opacity/scrollOpacity.directive.js
@@ -26,11 +26,15 @@
 let opacityAreaHeight = 420;
 let componentsSelector = 'article-card';
 
+const Window = new WeakMap();
+const Timeout = new WeakMap();
+
 class ScrollOpacity{
 
-    constructor($window, $timeout){
-        this.$window = $window;
+    constructor($document, $timeout){
+        this.$document = $document;
         this.$timeout = $timeout;
+
         this.restrict = 'A';
     }
 
@@ -42,42 +46,48 @@ class ScrollOpacity{
         componentsSelector = attrs.scrollOpacity || componentsSelector;
         opacityAreaHeight = attrs.opacityAreaHeight || opacityAreaHeight;
 
-        angular.element(this.$window ).on('scroll', this.scroll.bind(this));
-        angular.element(this.$window.document).ready( this.scroll.bind(this));
-
+        angular
+            .element(  this.$document )
+            .on('scroll', this.scroll.bind(this) )
+            .ready( this.scroll.bind(this) );
     }
 
     scroll(event) {
         const self = this;
         this.$timeout( () => {
 
-            const viewportHeight = self.$window.innerHeight;
+            const viewportHeight = self.$document[0].documentElement.clientHeight;
             const noOpacityAreaHeight = viewportHeight - opacityAreaHeight;
+            let components = [].slice.call( self.elem[0].querySelectorAll( componentsSelector ) );
 
-            let components = [].slice.call( self.elem[0].querySelectorAll(componentsSelector) );
             if (!components)  return;
 
             components.forEach( (component) => angular.element(component).css('opacity', 1 ) );
+
+            if ((viewportHeight + self.$document[0].body.scrollTop) >= self.$document[0].body.scrollHeight) {
+                return;
+            }
+
             let opacityComponents = components.filter((component) => {
                 const componentOffset = component.getBoundingClientRect().top;
                 return componentOffset > noOpacityAreaHeight && componentOffset < viewportHeight;
             });
 
-            const coefficientRate = 1 / (opacityComponents.length + 1);      // do +1 for a better opacity effect.
+            const coefficientRate = 1 / (opacityComponents.length + 1);                                                 // do +1 for a better opacity effect.
 
-            opacityComponents.reduce( (prevOpacity, currentValue, index, arr)=> {
-                angular.element(currentValue).css('opacity', prevOpacity);
+            opacityComponents.reduce( (prevOpacity, component) => {
+                angular.element( component ).css('opacity', prevOpacity);
                 return prevOpacity - coefficientRate;
             }, 1 - coefficientRate);
 
         });
     }
 
-    static entryPoint($window, $timeout){
-        return new ScrollOpacity($window, $timeout);
+    static entryPoint($document, $timeout){
+        return new ScrollOpacity($document, $timeout);
     }
 }
 
-ScrollOpacity.entryPoint.$inject = ['$window', '$timeout'];
+ScrollOpacity.entryPoint.$inject = ['$document', '$timeout'];
 
 export default ScrollOpacity.entryPoint;
